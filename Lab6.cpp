b0abb85a92f844e5d406cc0336d020acc657ed2c
AlexBarbachov <66849795+AlexBarbachov@users.noreply.github.com>
2025-09-13 17:48:50 -0700

    Second Commit, outputArray Done

--- Lab6.cpp
+++ Lab6.cpp
@@ -2,10 +2,19 @@
 
 using namespace std;
 
+
+double sumArray(double *arr, int size);
+void outputArrayData(double *arr, int size);
+double* enterArrayData(int size);
+
 int main() 
 {
-    double *numbers = enterArrayData(5);
-    
+    const int SIZE = 5;
+
+    double *numbers = nullptr;
+    numbers = enterArrayData(SIZE);
+
+
 
 
     // free up memory
@@ -14,12 +23,15 @@ int main()
 
 double sumArray(double *arr, int size)
 {
-    ;
+    
 }
 
 void outputArrayData(double *arr, int size)
 {
-    ;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Outputting array elements: " << arr[i] << " ";
+    }
 }
 
 double* enterArrayData(int size)
