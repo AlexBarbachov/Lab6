7aa2f77cbe060873be4e38934b693ad8d2c29f6d
AlexBarbachov <66849795+AlexBarbachov@users.noreply.github.com>
2025-09-13 17:38:15 -0700

    Second Commit

--- Lab6.cpp
+++ Lab6.cpp
@@ -4,15 +4,39 @@ using namespace std;
 
 int main() 
 {
+    double *numbers = enterArrayData(5);
+    
 
+
+    // free up memory
+    delete [] numbers;
 }
 
-double sumArray()
+double sumArray(double *arr, int size)
 {
-
+    ;
 }
 
-void outputArrayData()
+void outputArrayData(double *arr, int size)
 {
-
+    ;
 }
+
+double* enterArrayData(int size)
+{
+    // declare dynamic array
+    double *arr = new double[size];
+    double element = 0;
+
+
+    cout << "Data entry for the array: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Element #" << i << ":";
+        cin >> element;
+        arr[i] = element;
+    }
+
+    return arr;
+}
+
